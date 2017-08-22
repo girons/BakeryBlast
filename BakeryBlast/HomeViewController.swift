2efd2dfa9a387dbc134a1802e4eba49568ee0445
George Irons <george.irons@kwiff.com>
2017-08-22 16:46:13 +0100

    split homeBackground image into 4 slices, loading each consecutive image into scrollView and launching screen from bottom of scrollView

--- BakeryBlast/HomeViewController.swift
+++ BakeryBlast/HomeViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 import SpriteKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIScrollViewDelegate {
     var scene: HomeScene!
+    let scrollView = UIScrollView() // Create the scrollView
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -27,16 +29,58 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Configure the view.
-        let skView = view as! SKView
-        skView.isMultipleTouchEnabled = false
+//        // Configure the view.
+//        let skView = view as! SKView
+//        skView.isMultipleTouchEnabled = false
+//        
+//        // Create and configure the scene.
+//        scene = HomeScene(size: skView.bounds.size)
+//        scene.scaleMode = .aspectFill
+//        
+//        // Present the scene.
+//        skView.presentScene(scene)
         
-        // Create and configure the scene.
-        scene = HomeScene(size: skView.bounds.size)
-        scene.scaleMode = .aspectFill
+        // Set up and add scrollView to view
+        scrollView.frame = self.view.frame
+        self.scrollView.isPagingEnabled = true
+        self.view.addSubview(scrollView)
+        self.scrollView.delegate = self
         
-        // Present the scene.
-        skView.presentScene(scene)
+        self.scrollView.contentOffset = CGPoint(x:0, y: 4168)
+        
+        // An array of homeBackground images to add to the view
+        let x : [UIImage] = [UIImage(named: "homeBackground1")!,
+                             UIImage(named: "homeBackground2")!,
+                             UIImage(named: "homeBackground3")!,
+                             UIImage(named: "homeBackground4")!]
+        
+        // For each UIImage add a view
+        for index in 0...x.count-1 {
+            let subView = UIScrollView(frame: CGRect(
+                x:0,
+                y:(self.scrollView.frame.height * CGFloat(index)),
+                width:self.scrollView.frame.width,
+                height:self.scrollView.frame.height))
+            
+            // Set the size of the content view
+            let contentView = UIImageView(frame: CGRect(x:0, y:0, width:self.view.frame.width, height:self.view.frame.height))
+            
+            subView.contentSize = CGSize(width:self.view.frame.width, height:contentView.frame.height)
+            contentView.image = x[index]
+            subView.addSubview(contentView)
+            scrollView.addSubview(subView) // Add View
+        }
+        
+        let c = (self.scrollView.frame.size.height) * CGFloat(x.count)
+        self.scrollView.contentSize = CGSize(width:self.scrollView.frame.width, height:c)
+        
+        // Background Colour
+        self.view.backgroundColor = UIColor.gray
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 }
 
